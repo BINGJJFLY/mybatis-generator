cdd73ad93f2d8d623aee39f172bbb9b91152b099
wangjz <wangjinzhao@lionbridgecapital.cn>
2020-11-16 18:25:29 +0800

    添加Mybatis反向工程项目

---
+++ Mybatis-Reverse/src/main/java/com/itheima/shop/mapper/CouponMapper.java
@@ -0,0 +1,30 @@
+package com.itheima.shop.mapper;
+
+import com.itheima.shop.pojo.Coupon;
+import com.itheima.shop.pojo.CouponExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CouponMapper {
+    int countByExample(CouponExample example);
+
+    int deleteByExample(CouponExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Coupon record);
+
+    int insertSelective(Coupon record);
+
+    List<Coupon> selectByExample(CouponExample example);
+
+    Coupon selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Coupon record, @Param("example") CouponExample example);
+
+    int updateByExample(@Param("record") Coupon record, @Param("example") CouponExample example);
+
+    int updateByPrimaryKeySelective(Coupon record);
+
+    int updateByPrimaryKey(Coupon record);
+}
---
+++ Mybatis-Reverse/src/main/java/com/itheima/shop/pojo/Coupon.java
@@ -0,0 +1,66 @@
+package com.itheima.shop.pojo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Coupon {
+    private Long id;
+
+    private BigDecimal amount;
+
+    private Integer userId;
+
+    private Integer orderId;
+
+    private Integer isUsed;
+
+    private Date usedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getIsUsed() {
+        return isUsed;
+    }
+
+    public void setIsUsed(Integer isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public Date getUsedTime() {
+        return usedTime;
+    }
+
+    public void setUsedTime(Date usedTime) {
+        this.usedTime = usedTime;
+    }
+}
